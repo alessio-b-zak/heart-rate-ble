7fd273beef26c40d2d0c95140eb6c60994c0deb3
Stefan Klas <sk15962@bristol.ac.uk>
2018-02-06 23:29:56 +0000

    Heart beat reading

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 from bluepy import btle
+import struct
 
 
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
         btle.DefaultDelegate.__init__(self)
 
-    def handleNotifications(self, cHandle, data):
-        print("A notification was received: %s" %data)
+    def handleNotification(self, cHandle, data):
+        data_ints = struct.unpack('<' + 'B'*len(data), data)
+        print (data_ints)
+        print("Your heartbeat is : %d" %(data[1]))
 
 
 
@@ -14,19 +17,31 @@ class MyDelegate(btle.DefaultDelegate):
 
 def main():
     try:
-        heartrate = btle.Peripheral("d2:89:67:b7:bc:aa", btle.ADDR_TYPE_RANDOM)
-        heartrate.setDelegate(MyDelegate())
-        print("done")
-        # for descriptor in heartrate.getDescriptors():
-        #     if str(descriptor.uuid).startswith("00002902"):
+        heartrateP = btle.Peripheral("d2:89:67:b7:bc:aa", btle.ADDR_TYPE_RANDOM)
+        heartrateP.setDelegate(MyDelegate())
+        for characteristic in heartrateP.getCharacteristics():
+            #print(characteristic.properties);
+            print(characteristic.uuid);
+            print(characteristic.getHandle())
+
+
+        #for descriptor in heartrate.getDescriptors():
+        #if str(descriptor.uuid).startswith("00002902"):
         #         print(descriptor.valHandle)
-        heartrate.writeCharacteristic(15, "\x01\x00".encode())
+        # struct.pack('<bb', 0x01, 0x00)
+
+        print(heartrateP.writeCharacteristic(15, struct.pack('<bb',1,0),True));
+
         while True:
-            if heartrate.waitForNotifications(1.0):
+            if heartrateP.waitForNotifications(2.0):
                 print("waiting for notifications")
                 continue
+            else:
+                print("no notification found")
     except Exception as e:
         print(e)
+    finally:
+        heartrateP.disconnect()
 
 
 
