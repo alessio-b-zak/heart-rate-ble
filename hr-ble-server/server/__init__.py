7cd2884fab31501795905bb7be247c964f5a35c4
Alessio Zakaria <alessio.b.zakaria@googlemail.com>
2018-02-13 17:51:24 +0000

    Got it working

--- hr-ble-server/server/__init__.py
+++ hr-ble-server/server/__init__.py
@@ -2,32 +2,25 @@ from flask import Flask
 from bluepy import btle
 import struct
 import threading
-import atexit 
+import atexit
 
-
+commonDataStruct = {'peripheral' : None, 'heartrateP' : None}
+POOL_TIME = 1 #Seconds
+# lock to control access to variable
+dataLock = threading.Lock()
+# thread handler
+yourThread = threading.Thread()
 
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
         btle.DefaultDelegate.__init__(self)
 
     def handleNotification(self, cHandle, data):
-        global heartrateCurrent
+        global commonDataStruct
         data_ints = struct.unpack('<' + 'B'*len(data), data)
-        heartrateCurrent = data
-
-
-        #Averages number of beats per minute
-
-POOL_TIME = 5 #Seconds
-
-# variables that are accessible from anywhere
-commonDataStruct = {'peripheral' : None, 'heartrateP' : None}
-
-# lock to control access to variable
-dataLock = threading.Lock()
-# thread handler
-yourThread = threading.Thread()
-
+        print("handling notification")
+        print(data)
+        commonDataStruct['heartrateP'] = data_ints[1]
 
 
 def create_app():
@@ -42,19 +35,24 @@ def create_app():
         global yourThread
         with dataLock:
         # Do your stuff with commonDataStruct Here
-            if (not commonDataStruct['heartrateP'] == None):
-                commonDataStruct['heartrateP'] += 10
-            else:
-                commonDataStruct['heartrateP'] = 0
-
-        # Set the next thread to happen
-        yourThread = threading.Timer(POOL_TIME, doStuff, ())
-        yourThread.start()   
+            while True:
+                if commonDataStruct['peripheral'].waitForNotifications(2.0):
+                    continue
+                else:
+                    print("no notification found")
 
     def doStuffStart():
         # Do initialisation stuff here
         global yourThread
         # Create your thread
+        try:
+            commonDataStruct['peripheral'] = btle.Peripheral("d2:89:67:b7:bc:aa", btle.ADDR_TYPE_RANDOM)
+            commonDataStruct['peripheral'].setDelegate(MyDelegate())
+            # task = poll_heart_rate.delay()
+            commonDataStruct['peripheral'].writeCharacteristic(15, struct.pack('<bb',1,0),True)
+            print("Connected to peripheral")
+        except Exception as e:
+            print(e)
         yourThread = threading.Timer(POOL_TIME, doStuff, ())
         yourThread.start()
 
@@ -66,35 +64,9 @@ def create_app():
 
 server = create_app()
 
-#def poll_heart_rate():
-#    # while True:
-#    print("Hello")
-#    global heartrateCurrent
-#    heartrateCurrent += 20
-#        # if heartrateP.waitForNotifications(2.0):
-#        #     continue
-#        # else:
-#        #     print("no notification found")
-#
-#
-#
-#@server.before_first_request
-#def startup():
-#    global heartrateP
-#    try:
-#        # heartrateP = btle.Peripheral("d2:89:67:b7:bc:aa", btle.ADDR_TYPE_RANDOM)
-#        # heartrateP.setDelegate(Deleate())
-#        task = poll_heart_rate.delay()
-#    except Exception as e:
-#        print(e)
-
-
 def calculate_heart():
     global commonDataStruct
     return str(commonDataStruct['heartrateP'])
-
-
-
 
 @server.route('/')
 def index():
